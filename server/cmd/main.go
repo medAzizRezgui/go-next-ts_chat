960fb53e19e2773f4761e3c79d6e7cc32701f692
Root99 <azizrezgui4@gmail.com>
2023-07-04 22:30:52 +0100

    added router

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"log"
 	"server/db"
+	"server/internal/user"
+	"server/router"
 )
 
 func main() {
-	_, err := db.NewDataBase()
+	dbConn, err := db.NewDataBase()
 	if err != nil {
-		log.Fatalf("Error: %s", err)
+		log.Fatalf("Could not initialize database connection : %s", err)
 	}
+
+	userRep := user.NewRepository(dbConn.GetDB())
+	userSvc := user.NewService(userRep)
+	userHandler := user.NewHandler(userSvc)
+
+	router.InitRouter(userHandler)
+	router.Start("0.0.0.0:8080")
 }
---
+++ server/router/router.go
@@ -0,0 +1,16 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"server/internal/user"
+)
+
+var r *gin.Engine
+
+func InitRouter(userHandler *user.Handler) {
+	r = gin.Default()
+	r.POST("/signup", userHandler.CreateUser)
+}
+func Start(addr string) error {
+	return r.Run(addr)
+}
